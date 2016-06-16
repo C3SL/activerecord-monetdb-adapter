156324f523e5e94ec3d0bf030bc4c8cb66a600f1
João Victor Risso <jvtr12@inf.ufpr.br>
2016-06-16 10:24:42 -0300

    Revert change in port conversion
    Signed-off-by: João Victor Risso <jvtr12@inf.ufpr.br>

--- lib/active_record/connection_adapters/monetdb_adapter.rb
+++ lib/active_record/connection_adapters/monetdb_adapter.rb
@@ -18,11 +18,13 @@ module ActiveRecord
 
       client = MonetDB.new
       config[:host] = config.delete(:hostname) || "127.0.0.1"
-      config[:port] = config.delete(:port).to_i || 50000
+      config[:port] = config.delete(:port).to_s || "50000"
       config[:user] = config.delete(:username) || "monetdb"
       config[:passwd] = config.delete(:password) if config[:password]
       config[:db_name] = config.delete(:database) if config[:database]
       config[:auth_type] = config[:auth_type] || "SHA1"
+
+      logger.log(config)
 
       begin
         client.connect(user = config[:user], passwd = config[:passwd], host = config[:host], port = config[:port], database_connection_name = config[:db_name], auth_type = config[:auth_type])
